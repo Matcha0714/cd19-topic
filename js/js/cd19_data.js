8d3c5bb2381f9bd24a21d855ee8f6a1edd6ad95f
Matcha0714 <slimetw0714@gmail.com>
2022-05-19 11:27:18 +0800

    covid

--- js/js/cd19_data.js
+++ js/js/cd19_data.js
@@ -1,35 +1,52 @@
 $(function(){
-    var color = ['#63a745', '#17a2b8', '#343a40', '#dc3545'];
-    var title = ['累計確診', '新增確診', '累計死亡', '台灣疫苗涵蓋率'];
-
-    $(document).ready(() => {
-        $.getJSON('http://cs102.nihs.tp.edu.tw:5050/job1').then((res) => { console.log(res);
-            for (let i = 0; i < 4; i++) {
-                createData(title[i], color[i], res[0][i+1], i);
-
-                if (i == 2){
-                    $(`#cd19-box2`).append(`<span style="font-size: 10px; font-family: Arial, Helvetica, sans-serif;">(${res[0][5]})</span>`);
-                }
-            }
-        })
-    })
-    function createData(title='標題', color='#000', text='123', pos){
-        $('#js-cd19-data').append(`
-            <div id="cd19-box${pos}" class="text-center m-auto">
-                <div style="color:#555555">${title}</div>
-                <span style="color: ${color}; font-size: 35px;" class="fw-bold">${text}</span>
-            </div>
-        `); 
-    }
-
-    // test
+    var title = [
+        ['累計確診', '新增確診', '累計死亡', '台灣疫苗涵蓋率'],
+        ['累計確診', '新增確診', '第一劑', '第二劑']
+    ];
     var res = [
         ['台灣','896,059', '+65,833', '1,135', '86.24%','+38'],
-        ['新北','123,459', '+36,213', '896', '85.13%','+10'],
-        ['台北','123,456', '+36,213', '896', '85.13%','+10']
+        ['台北','123,656', '+36,21', '15', '85.23%','+12'],
+        ['新北','123,459', '+36,213', '896', '85.13%','+10']
     ];
-    for (let i = 0; i < 4; i++) {
-        createData(title[i], color[i], res[0][i+1], i);
-        if (i == 2) $(`#cd19-box2`).append(`<span style="font-size: 10px; font-family: Arial, Helvetica, sans-serif;">(${res[0][5]})</span>`);
+
+    var cd19_data_box, cd19_box_top, cd19_box_bottom, cd19_box_2;
+    var btn_choose_county = $(".btn-choose-county");
+    var btn_navbar_brand = $(".navbar-brand");
+
+    $.getJSON('http://cs102.nihs.tp.edu.tw:5050/job1').then((res) => { console.log(res);
+
+    });
+    
+    function create_element(){
+        var color = ['#63a745', '#17a2b8', '#343a40', '#dc3545'];
+
+        for(let i=0; i<4; i++){
+            $('#js-cd19-data').append(`
+                <div class="cd19_data_box text-center m-auto">
+                    <div class="cd19_box_top" style="color:#555555">${title[0][i]}</div>
+                    <span class="cd19_box_bottom fw-bold" style="color:${color[i]}; font-size: 35px;">${res[0][i+1]}</span>
+                </div>
+            `);
+        }
+        $(`#js-cd19-data > div`).eq(2).append(`<span class="cd19_box_2" style="font-size: 10px; font-family: Arial, Helvetica, sans-serif;">(${res[0][5]})</span>`);
+
+        cd19_data_box = $('.cd19_data_box');
+        cd19_box_top = $('.cd19_box_top');
+        cd19_box_bottom = $('.cd19_box_bottom');
+        cd19_box_2 = $('.cd19_box_2');
     }
+
+    function btn_update(pos){
+        for(let i=0; i<4; i++){
+            cd19_box_top.eq(i).text(title[1][i]);
+            cd19_box_bottom.eq(i).text(res[pos][i+1]);
+        }
+        cd19_box_2.text(`(${res[pos][5]})`);
+        btn_navbar_brand.text(res[pos][0]);
+    }
+
+    btn_choose_county.click(function(){ btn_update(btn_choose_county.index(this)+1); });
+
+    create_element();
 });
+
