44b72026069076d9c475fffcc3aedf40191ba792
Matcha0714 <slimetw0714@gmail.com>
2022-05-31 11:07:22 +0800

    covid

--- js/js/cd19_data.js
+++ js/js/cd19_data.js
@@ -1,13 +1,8 @@
 
-
-
 $(function(){
     var cd19_data = $('#js-cd19-data');
-    var cd19_data_box, cd19_box_top, cd19_box_bottom;
-    var btn_choose_county = $(".btn-choose-county");
-    var btn_navbar_brand = $(".navbar-brand");
 
-
+    var color = ['#63a745', '#17a2b8', '#343a40', '#dc3545'];
     var title = [
         ['累計確診', '新增確診', '總接種人次', '台灣疫苗涵蓋率'],
         ['累計確診', '新增確診', '第一劑', '第二劑']
@@ -21,48 +16,39 @@ $(function(){
         ['桃園市','153,382', '+9,658', '87.1%', '82.4%']
     ];
 
-    $.getJSON('http://cs102.nihs.tp.edu.tw:5050/job1').then((res) => { console.log(res);
-
-    });
-
-
-
-    function create_button(){
-        var btn = '<button type="button" class="btn-choose-county btn btn-outline-secondary" style="margin: 0px 5px 10px 5px;"></button>';
-
-        for(let i=1; i<5; i++){
-            var test = $('<button type="button" class="btn-choose-county btn btn-outline-secondary" style="margin: 0px 5px 10px 5px;"></button>')
-
-            cd19_data.find('div').eq(0).find('div').append(test);
-        }
+    // Initial Element
+    // cd19 data
+    for(let i=0; i<4; i++){
+        $('#js-cd19-data').find('div').eq(3).append(`
+            <div class="cd19_data_box text-center m-auto">
+                <div class="cd19_box_top" style="color:#555555">${title[0][i]}</div>
+                <span class="cd19_box_bottom fw-bold" style="color:${color[i]}; font-size: 35px;">${res[0][i+1]}</span>
+            </div>
+        `);
     }
-    
-    function create_element(){
-        var color = ['#63a745', '#17a2b8', '#343a40', '#dc3545'];
-
-        for(let i=0; i<4; i++){
-            $('#js-cd19-data').append(`
-                <div class="cd19_data_box text-center m-auto">
-                    <div class="cd19_box_top" style="color:#555555">${title[0][i]}</div>
-                    <span class="cd19_box_bottom fw-bold" style="color:${color[i]}; font-size: 35px;">${res[0][i+1]}</span>
-                </div>
-            `);
-        }
-        cd19_data_box = $('.cd19_data_box');
-        cd19_box_top = $('.cd19_box_top');
-        cd19_box_bottom = $('.cd19_box_bottom');
+    // button
+    for(let i=1; i<=5; i++){
+        var btn = $(`<button type="button" class="btn-choose-county btn btn-outline-secondary" style="margin: 0px 5px 10px 5px;">${res[i][0]}</button>`);
+        cd19_data.find('div').eq(0).find('div').append(btn);
     }
 
-    function btn_update(pos){
+    var cd19_data_box= $('.cd19_data_box'), cd19_box_top = $('.cd19_box_top'), cd19_box_bottom = $('.cd19_box_bottom');
+    var btn_choose_county = $(".btn-choose-county");
+    var btn_navbar_brand = $(".navbar-brand");
+
+    // button update cd19 data
+    btn_choose_county.click(function(){
+        let pos = btn_choose_county.index(this)+1;
+
         for(let i=0; i<4; i++){
             cd19_box_top.eq(i).text(title[1][i]);
             cd19_box_bottom.eq(i).text(res[pos][i+1]);
         }
         btn_navbar_brand.text(res[pos][0]);
-    }
+    });
 
-    btn_choose_county.click(function(){ btn_update( btn_choose_county.index(this)+1); });
+    $.getJSON('http://cs102.nihs.tp.edu.tw:5050/job1').then((res) => { console.log(res);
 
-    create_button();
+    });
 });
 
